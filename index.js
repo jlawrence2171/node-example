5b449dd83147d1e31d4ea6733b865d1e4e4735e0
jlawrence2171 <81828527+jlawrence2171@users.noreply.github.com>
2021-07-01 16:59:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let lodash = require('lodash');
+
+let name = "jeff";
+
+upperCase = lodash.upperFirst(name);
+lowerCase = lodash.lowerFirst(name);
+// startCase = lodash.startFirst(name);
+
+console.log(upperCase);
+console.log(lowerCase);
+// console.log(startCase);
+
+//use these functions from lodash:
+// lowerCase();
+// uppCase();
+// startCase();
